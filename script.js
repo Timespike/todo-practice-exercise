00435d9941babad6e8bffdc4296fea2ae8d9d734
unknown <timespike@yahoo.com>
2022-01-17 15:42:52 -0600

    troubleshooting script issues

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const inputfield = document.getElementById('entry');
+const submitButton = document.getElementById('submit');
 
 console.log('inputfield');
 
 // Add Items to the list
 
+function buttonTest() {
+    alert("button clicked");
+}
+
 inputfield.addEventListener('click', addListItem());
+submitButton.addEventListener('click', buttonTest());
