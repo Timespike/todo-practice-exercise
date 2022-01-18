a0ec545c417a4bb48dec1cc1763a1f7b52956214
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-17 21:22:53 -0600

    still trying to figure out what I'm missing here

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const inputfield = document.getElementById('entry');
-const pushMe = document.getElementById('submit');
+const pushMe = document.getElementById('pushMe');
 
 console.log('inputfield');
 
 // Add Items to the list
 
 function buttonTest() {
-    alert("button clicked");
     console.log("button clicked");
 }
 
-inputfield.addEventListener('click', addListItem());
 pushMe.addEventListener('click', buttonTest());
