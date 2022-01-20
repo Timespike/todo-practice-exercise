a05f682e7e0e142f6a23616ab82fa4d44e706b65
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-19 22:29:24 -0600

    got the one thing working, at least

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const pushMe = document.getElementById('pushMe');
 
-console.log('inputfield');
-
 // Add Items to the list
 
 function buttonTest() {
     console.log("button clicked");
 }
 
-pushMe.addEventListener('click', buttonTest());
+pushMe.addEventListener('click', buttonTest);
