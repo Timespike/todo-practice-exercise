ca31220e90c3522279152c46afa37d9e39364cae
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-24 17:30:14 -0600

    working on making input as convenient as possible

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const submitBtn = document.getElementById('submit-button');
 const entry = document.getElementById('entry');
 const list = document.querySelector("ul");
 
+function isEnter(e) {
+    if (e.which === 13) {
+        buttonTest()
+    } 
+}
+
 function buttonTest() {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(entry.value));
-    list.appendChild(li, <button>Delete</button>);
+    if (entry.value.length > 0 && !entry.value.startsWith(' ')) {
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(entry.value));
+        list.appendChild(li);
+        entry.value = "";
+    }
 }
 
 submitBtn.addEventListener('click', buttonTest);
+entry.addEventListener('keypress', isEnter(e));
