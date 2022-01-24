8f95b3b71e3828719935fab83ca8d7e7268d63ba
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-24 16:56:13 -0600

    looked up how to get the adds working

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const submitBtn = document.getElementById('submit-button');
 const entry = document.getElementById('entry');
+const list = document.querySelector("ul");
 
 function buttonTest() {
-    console.log("button clicked");
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(entry.value));
+    list.appendChild(li, <button>Delete</button>);
 }
 
 submitBtn.addEventListener('click', buttonTest);
