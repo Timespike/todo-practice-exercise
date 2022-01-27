69afd1a3012af5f43511f102c4139dd1010ef6da
unknown <timespike@yahoo.com>
2022-01-27 14:04:16 -0600

    got the strikethru working

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const submitBtn = document.getElementById('submit-button');
 const entry = document.getElementById('entry');
 const list = document.querySelector("ul");
 
+// function toggleDone () {
+//     this.classList.toggle("done");
+// }
+
 function addListEntry() {
     // checks to make sure there's something to enter and it doesn't start with a space
     if (entry.value.length > 0 && !entry.value.startsWith(' ')) {
         // creates list item and delete button
         let li = document.createElement("li");
-        li.innerHTML = `<div>${entry.value} <button>Delete</button></div>`;
+        li.innerHTML = `<div class="list-entry" onclick="this.classList.toggle('done')">${entry.value} <button>Delete</button></div>`;
         list.appendChild(li);
         entry.value = "";
     }
@@ -20,4 +24,3 @@ entry.addEventListener('keydown', (e) => {
     addListEntry();
     }
 });
-
