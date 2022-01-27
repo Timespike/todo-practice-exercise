3acbfd588bd480e48cc9209c5ede9355a4148b1c
unknown <timespike@yahoo.com>
2022-01-27 12:53:52 -0600

    added a comment

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const list = document.querySelector("ul");
 function addListEntry() {
     // checks to make sure there's something to enter and it doesn't start with a space
     if (entry.value.length > 0 && !entry.value.startsWith(' ')) {
+        // creates list item and delete button
         let li = document.createElement("li");
         li.innerHTML = `<div>${entry.value} <button>Delete</button></div>`;
         list.appendChild(li);
